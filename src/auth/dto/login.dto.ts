8be78d89449997ba1612024149ee80f0260ba03e
olivierbretaud <olivierbretaud@hotmail.fr>
2026-01-20 17:07:31 +0100

    fix(loginDto) emai.property

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsEmail } from 'class-validator';
-import { ApiEmailProperty } from 'src/swagger/email.property';
+import { ApiEmailProperty } from '../../swagger/email.property';
 
 
 export class loginDto {
